fd815a96b0142406781592f9759126ffb6ed92c0
Ahmad Rifa'i <ahmadrifai716@gmail.com>
2024-07-02 11:44:10 +0700

    user page

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('tailwind-testing', function () {
-    return view('app');
-});
+})->name('home');
 
 Route::get('login', function () {
     return view('login');
-});
+})->name('login');
 
 Route::get('/admin/dashboard', function () {
     return view('dashboard');
-});
+})->name('dashboard');
+
+Route::get('/admin/user', function () {
+    return view('user');
+})->name('user');
+
+Route::get('/admin/product', function () {
+    return view('product');
+})->name('product');
