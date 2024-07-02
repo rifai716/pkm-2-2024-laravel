49083b12e950795e5fb8cf36c1f01045c18cf67f
Ahmad Rifa'i <ahmadrifai716@gmail.com>
2024-07-02 11:20:10 +0700

    remove bg

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -2,7 +2,7 @@
 @section('body')
     @include('navbar')
     @include('sidebar')
-    <div class="p-4 sm:mt-14 sm:ml-64 bg-slate-400">
+    <div class="p-4 sm:mt-14 sm:ml-64">
         @yield('content')
     </div>
 @endsection
